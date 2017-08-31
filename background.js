d1fc037b4261e4182ef0283cb0d1b53c997918c5
Garrett Estrin <garrett.estrin@gmail.com>
2017-08-30 22:01:57 -0700

    adding base functionality. build img element and add click listener. scoreboard in localStorage

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log('background js loaded');
-
-chrome.tabs.query({active: true, 'currentWindow': true}, function(tabs) {
-    activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, {"message": "sent_url", "url": url, "localStorage": LS});
-    url = "";
-    params = "";
-    value = "";
-});
--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-// console.log('js loaded');
+console.log('js loaded');
+
+// function that runs when you find AJ!
+function foundHer(){
+  console.log('You found her!');
+  var child = document.getElementById("ajpng");
+  child.parentNode.removeChild(child);
+}
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if( request.message === "sent_url" ) {
-      console.log(message)
+    if( request.message === "true" ) {
+      body = document.getElementsByTagName('body')[0];
+      node = document.createElement('div');
+      node.setAttribute('class', 'imageOfAj');
+      content = document.createElement('img');
+      content.setAttribute('src', 'https://s3-us-west-2.amazonaws.com/garrettbeer/logo-large.png');
+      content.setAttribute('id', 'ajpng');
+      content.setAttribute('style', 'position:absolute;left:50%;top:50%;height:10px;width:10px;background-color:red;')
+      node.appendChild(content);
+      body.appendChild(node);
+      document.getElementById('ajpng').addEventListener('click', foundHer)
     }
   }
 );
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "setup" ) {
+      console.log(request.setup)
+    }
+  }
+);
+
+
+
 
